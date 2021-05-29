1319c73eaa4ea3ef42bb3015b12f9bf7d0cc70c3
Ashutosh Kumar <aksamtiwari@gmail.com>
2021-05-30 03:31:26 +0530

    Update settings.py

--- Covid_Support/settings.py
+++ Covid_Support/settings.py
@@ -121,6 +121,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#Added Manually
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
