aaf54ad0a75ed3329305d5899b345ca0fb601541
HARIHARAN B P <bphariharan1301@gmail.com>
2021-06-02 11:08:30 +0530

    Django Secret Key Hidden

--- Covid_Support/settings.py
+++ Covid_Support/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+!pratnbv*uahte_k3@)(r_s_@m2xh1cm2ay@43jdq_d5t8nu'
+# SECRET_KEY = 'django-insecure-4+!pratnbv*uahte_k3@)(r_s_@m2xh1cm2ay@43jdq_d5t8nu'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
