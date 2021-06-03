7785fc57082e247d31837d2383fea9a57f6c3900
HARIHARAN B P <bphariharan1301@gmail.com>
2021-06-03 19:17:45 +0530

    Static File Folder created and Design Changes

--- Covid_Support/settings.py
+++ Covid_Support/settings.py
@@ -140,12 +140,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Tells where after performing collectstatic
 STATIC_ROOT = 'Covid_Support/static'
 
 #Added Manually
+# Tells where the static files are locating before performing collectstaic 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
