c97921854765310c3cea555263f8addae070bc95
HARIHARAN B P <bphariharan1301@gmail.com>
2021-06-22 21:40:27 +0530

    Heroku Sttings

--- Covid_Support/settings.py
+++ Covid_Support/settings.py
@@ -26,13 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+DEBUG = False
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DEBUG')
+# DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
